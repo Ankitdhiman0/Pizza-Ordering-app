ccb39b8dc5634eeb23d830e9d2644c49785c60ab
Ankit <ankitdhiman4567@gmail.com>
2025-07-09 00:40:12 +0530

    UI bugs removed

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="flex flex-wrap items-center justify-between gap-2 border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:flex-nowrap sm:px-6">
       <Link to="/" className="tracking-widest text-sm sm:text-base">
-        Fast React Pizza Co.
+        Ankit's Pizza Co.
       </Link>
 
       <SearchOrder />
